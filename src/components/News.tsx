7ed063ce71f74e1c6dd15d5fa345146b3e46ff0e
mervegk <gekemeke123@gmail.com>
2025-01-06 00:41:14 +0300

    News: added search input

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { Input } from './ui/input'
 
 type Props = {}
 
 export default function News({ }: Props) {
   return (
-    <aside>News</aside>
+    <aside className='flex flex-col p-3'>
+      <Input className='bg-gray-100 rounded-full' placeholder='Search' />
+    </aside>
   )
 }
